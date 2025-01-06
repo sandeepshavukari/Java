ffc58aa3e57b310a170fe06756470fbcc0b434ae
SHAVUKARI SANDEEP <sandy20499@gmail.com>
2025-01-06 17:00:27 +0530

    ArrayList

---
+++ Java/src/com/sandeep/Collections/ArrayList/ArrayListExample.java
@@ -0,0 +1,10 @@
+package com.sandeep.Collections.ArrayList;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+	public static void main(String args[]) {
+		ArrayList al= new ArrayList();
+		al.add("sandeep");
+	}
+}
