b8633ebf4de2a8929ee02bb9cf657746446e2e67
SHAVUKARI SANDEEP <sandy20499@gmail.com>
2025-01-07 15:12:31 +0530

    Vector

---
+++ Java/src/com/sandeep/Collections/Vectors/VectorGenerics.java
@@ -0,0 +1,21 @@
+package com.sandeep.Collections.Vectors;
+
+import java.util.Vector;
+
+public class VectorGenerics {
+
+	public static void main(String[] args) {
+		Vector<Integer> v1= new Vector<>();
+		v1.add(1);
+		v1.add(2);
+		v1.add(3);
+		v1.add(7);
+		int sum=0;
+		for(int i:v1)
+		{
+			sum+=i;
+		}
+		System.out.println(sum);
+	}
+
+}
